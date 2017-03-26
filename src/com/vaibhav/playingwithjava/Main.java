d9ab394bf9e3d5116d4b9e877676fb87aa03a0a6
Vai Mor <mor.vaibhav577@gmail.com>
2017-03-26 13:29:39 +1300

    Playing around with conditional logic

--- src/com/vaibhav/playingwithjava/Main.java
+++ src/com/vaibhav/playingwithjava/Main.java
@@ -9,5 +9,15 @@ public class Main {
         int result = a < b ? a + b : 0;
 
         System.out.println(result);
+
+        int c = 50;
+
+        if(a < b) {
+            System.out.println("a is less than b");
+        }
+
+        if(a < c) {
+            System.out.println("a is less than c");
+        }
     }
 }
