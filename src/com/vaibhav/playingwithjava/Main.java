afb53d759306bd87189d590c9afd4c2b9145eae5
Vai Mor <mor.vaibhav577@gmail.com>
2017-03-26 13:23:59 +1300

    Used conditional assignment

--- src/com/vaibhav/playingwithjava/Main.java
+++ src/com/vaibhav/playingwithjava/Main.java
@@ -3,6 +3,11 @@ package com.vaibhav.playingwithjava;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a = 23;
+        int b = 47;
+
+        int result = a < b ? a + b : 0;
+
+        System.out.println(result);
     }
 }
